5d5e75bf398ed57cefba7cfa9c3b59564f6ce676
Aditya Ashok <aditya_ashok@hotmail.com>
2025-03-02 15:15:09 -0800

    Acquired Stock Data
    Used YFinance library to acquire data for publicly traded stocks and calculated log-returns
    
    Co-authored-by: Shashank Chidige <CheetahJokes@users.noreply.github.com>

--- scraper.py
+++ scraper.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import matplotlib.pyplot as plt
 
 class DataScraper:
     def __init__():
@@ -8,3 +12,18 @@ class DataScraper:
 
     def get_collection(self, stock_list, period_start, period_end):
         pass
+
+
+if __name__ == "__main__":
+    dat = yf.Ticker("MSFT")
+    one_mo_hist = dat.history(period = '6mo')[['Open', 'High', 'Low', 'Close']]
+    log_returns = np.log(one_mo_hist['Close'] / one_mo_hist['Close'].shift(1))
+    # scaled_vol = (one_mo_hist['High'] - one_mo_hist['Low'])/one_mo_hist['Low']
+    # Plot the histogram of log returns
+    plot = log_returns.dropna().plot.hist(bins=12, alpha=0.5)
+    plot.plot()
+    # Print the log returns
+    print(log_returns)
+    
+    # Show the plot
+    plt.show()
