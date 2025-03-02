0f74c08411bef47f8fe3830a4a8435abae34ae11
ashoka-man <63413643+ashoka-man@users.noreply.github.com>
2025-03-02 14:28:34 -0800

    Initial Commit
    Co-authored-by: Shashank Chidige <CheetahJokes@users.noreply.github.com>

---
+++ scraper.py
@@ -0,0 +1,10 @@
+
+class DataScraper:
+    def __init__():
+        pass
+
+    def get_stock(self, stock_name, period_start, period_end):
+        pass
+
+    def get_collection(self, stock_list, period_start, period_end):
+        pass
