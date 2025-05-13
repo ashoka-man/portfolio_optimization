5a47e41a58ebbc3c366079f348ceebfb0b0c83c7
Aditya Ashok <aditya_ashok@hotmail.com>
2025-05-13 10:32:29 -0700

    Factored in absolute shift in security prices.
    Will implement two seperate distributions in next commit.

--- bayesian.py
+++ bayesian.py
@@ -33,7 +33,7 @@ class ConjugatePriorOptimizer():
         return samples
 
 
-    def _kl_divergence(self, p_mu, p_sigma, q_mu, q_sigma):
+    def _kl_divergence(self, p_mu, p_sigma, q_mu, q_sigma, penalty = 0.2):
         """KL divergence D_KL(P || Q) for two multivariate normals"""
         d = p_mu.shape[0]
         inv_q_sigma = np.linalg.inv(q_sigma)
@@ -41,14 +41,22 @@ class ConjugatePriorOptimizer():
 
         term1 = np.trace(inv_q_sigma @ p_sigma)
         term2 = diff.T @ inv_q_sigma @ diff
-        term3 = np.log(np.linalg.det(q_sigma) / np.linalg.det(p_sigma))
+        term3 = np.log2(np.linalg.det(q_sigma) / np.linalg.det(p_sigma))
+
+        prop_diff = 0
+
+        # FULLY DEPENDENT ON WHETHER WE FEED PROPORTIONS ON REAL PRICES
+        for i in range(d):
+            prop_diff += np.linalg.norm((p_mu[i]/q_mu[i]) * q_mu - p_mu) ** 2
+
+        # proportional difference calculation
         
-        return 0.5 * (term1 + term2 - d + term3)
+        return 0.5 * (term1 + term2 + term3 - d) + penalty * prop_diff
 
     # Employ JS-Divergence to determine the divergence between the current 
     def compute_js_divergence(self):
         # DO MONTE CARLO TO ESIMATE JS-DIVERGENCE 
-        num_samples = 500
+        num_samples = 10000
         
         # Using the same sample from mixture distribution for comparison between new and prev
         prev_dist_samples = self._sample_niw(self.previous_mean_returns, self.previous_covariance_returns, self.previous_num_observations, num_samples)
@@ -66,7 +74,6 @@ class ConjugatePriorOptimizer():
             mu_c, cov_c = curr_dist_samples[i]
             mu_m, cov_m = mixture_dist_samples[i]
 
-
             kl_pm = self._kl_divergence(mu_p, cov_p, mu_m, cov_m)
             kl_cm = self._kl_divergence(mu_c, cov_c, mu_m, cov_m)
 
@@ -74,7 +81,19 @@ class ConjugatePriorOptimizer():
 
         return np.mean(jsd_samples)
 
+    # RETURN TO THIS TO FIND GOOD HYPERPARAMETERS
+    def compute_normalized_js_divergence(self):
+        return 1/(1 + np.exp(-1 * self.compute_js_divergence()))
+
 if __name__ == '__main__':
-    opt = ConjugatePriorOptimizer(20, np.array([1, 2, 3]), np.array([[0.1,0.5,0.0],[0.5,0.2,0.0],[0.0,0.0,0.3]]))
-    opt.update_prior(np.array([9, 10, 11]))
-    print(opt.compute_js_divergence())
+
+    mean = np.array([0.05, 0.1, 0.2])  # Mean vector (e.g., expected returns)
+    cov = np.array([                  # Covariance matrix (must be symmetric and PD)
+        [0.005, 0.001, 0.0003],
+        [0.001, 0.006, 0.0005],
+        [0.0003, 0.0005, 0.007]
+    ])
+
+    opt = ConjugatePriorOptimizer(20, mean, cov)
+    opt.update_prior(np.array([0.06, 0.08, 0.25]))
+    print(opt.compute_js_divergence()) 
